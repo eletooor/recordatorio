4dfa33e9d78ef5530ad24b7bacf26828bd659da0
Héctor González <hectorgonzalezuno@gmail.com>
2021-01-01 23:52:17 -0300

    first commit

---
+++ hlg.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+mkdir hlg
+cd hlg
+echo color 02 > texto.bat
+echo pause >> texto.bat
+echo exit >> texto.bat
+echo *******************inica****************************
+
+texto.bat
+echo *******************retorna**************************
+pause
